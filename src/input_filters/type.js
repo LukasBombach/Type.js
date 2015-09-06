4e5d727aa266a3b9f1bed12db8f74b3498b7ef57
Lukas Bombach <lukas.bombach@googlemail.com>
2015-09-06 23:25:25 +0100

    Building event system

--- src/input_filters/type.js
+++ src/input_filters/type.js
@@ -6,14 +6,19 @@
  * @constructor
  */
 function TypeFilter(type) {
+  this._keys = {};
 }
 
 (function() {
 
   /**
+   * Searches for a callback in the _keys map of this
+   * filter and calls it with the given event. Will also
+   * check for an 'all' definition as a fallback.
    *
-   * @param {KeydownEvent} e
-   * @returns {TypeFilter}
+   * @param {KeydownEvent} e - The event to apply the
+   *     filter for.
+   * @returns {TypeFilter} - This instance
    */
   this.apply = function(e) {
     var func = this._keys[e.key] || this._keys.all;
