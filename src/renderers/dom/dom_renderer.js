ed2becb86dc50826c9e71b265d9c3191ea878d9d
Lukas Bombach <lukas.bombach@googlemail.com>
2016-01-17 18:09:24 +0100

    Setting up renderer

--- src/renderers/dom/dom_renderer.js
+++ src/renderers/dom/dom_renderer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Renderer from '../renderer';
+import DomRendererCache from './dom_renderer_cache';
 
 /**
  * @augments Renderer
@@ -14,6 +15,8 @@ export default class DomRenderer extends Renderer {
   constructor(type) {
     super(type);
     this._el = type.getEl();
+    this._cache = new DomRendererCache();
+    this._currentlyRenderedNodes = {};
   }
 
   /**
@@ -21,7 +24,31 @@ export default class DomRenderer extends Renderer {
    * @returns {DomRenderer}
    */
   render() {
+
+    // Load all nodes that need to be rendered
+    const nodesToRender = this._cache.getOrCreateNodesForDocument(this._type.getDocument());
+    let lastIdInCurrentlyRenderedNodes = null;
+
+    // Iterate nodes, skip nodes that are already there, append new nodes to the dom
+    for (let id in nodesToRender) {
+      if (!nodesToRender.hasOwnProperty(id)) continue;
+      if (id in this._currentlyRenderedNodes) lastIdInCurrentlyRenderedNodes = id;
+      else this._renderNode(nodesToRender[id], this._currentlyRenderedNodes[id]);
+      delete this._currentlyRenderedNodes[id];
+    }
+
+    // Remove all nodes from dom that were previously rendered and not present anymore
+    for (let id in this._currentlyRenderedNodes) {
+      if (!this._currentlyRenderedNodes.hasOwnProperty(id)) continue;
+      this._removeNodeFromDom(this._currentlyRenderedNodes[id]);
+    }
+
+    // Reference currently rendered nodes for next rendering
+    this._currentlyRenderedNodes = nodesToRender;
+
+    // Chaining
     return this;
+
   }
 
   /**
---
+++ src/renderers/dom/dom_renderer_cache.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import DomRenderer from './dom_renderer';
+import BlockNode from '../../document/block_node';
+import TextNode from '../../document/text_node';
+
+export default class DomRendererCache {
+
+  /**
+   * Initializes the cache data structure
+   * @constructor
+   */
+  constructor() {
+    this._cache = {};
+  }
+
+  /**
+   * Returns a RenderNode from the cache or null if the RenderNode
+   * cannot be found. To retrieve a RenderNode pass the corresponding
+   * ID of the DocumentNode
+   *
+   * @param {number} documentNodeId
+   * @returns {RenderNode|null}
+   */
+  get(documentNodeId) {
+    return this._cache[documentNodeId] || null;
+  }
+
+  /**
+   * Writes a RenderNode to the cache identified by the ID of the
+   * corresponding DocumentNode
+   *
+   * @param {number} documentNodeId
+   * @param {RenderNode} renderNode
+   * @returns {DomRendererCache}
+   */
+  set(documentNodeId, renderNode) {
+    this._cache[documentNodeId.toString()] = renderNode;
+    return this;
+  }
+
+  /**
+   * Returns whether or not a given document node is in the cache
+   *
+   * @param documentNodeId
+   * @returns {boolean}
+   */
+  isCached(documentNodeId) {
+    return documentNodeId.toString() in this._cache;
+  }
+
+  /**
+   *
+   * @param {DocumentNode} documentNode
+   * @returns {RenderNode}
+   */
+  getOrCreateForDocumentNode(documentNode) {
+
+    let cachedItem = this.get(documentNode.id);
+
+    if (!cachedItem) {
+      cachedItem = DomRenderer.getRenderNodeFor(documentNode);
+      this.set(documentNode.id, cachedItem);
+    }
+
+    return cachedItem;
+  }
+
+  /**
+   *
+   * @param {TypeDocument} document
+   * @returns {{}}
+   */
+  getOrCreateNodesForDocument(document) {
+
+    const documentNodes = document.getNodes();
+    const len = documentNodes.length;
+    const renderNodes = {};
+    let id;
+
+    for (let i = 0; i < len; i++) {
+      id = documentNodes[i].id.toString();
+      renderNodes[id] = this.getOrCreateForDocumentNode(documentNodes[i]);
+    }
+
+    return renderNodes;
+
+  }
+
+}
